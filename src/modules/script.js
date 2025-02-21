d8f5aa7c72bfe06c366f70504609230a63a48945
Mykyta1003 <kn23-m.trynko@nubip.edu.ua>
2025-02-21 21:06:49 +0200

    Delete src/modules directory

--- src/modules/script.js
+++
@@ -1,8 +0,0 @@
-import { loadImages } from './api.js';
-import { clearGallery, removeLastImage, reverseGallery } from './gallery.js';
-
-loadImages();
-
-document.getElementById('clear').addEventListener('click', clearGallery);
-document.getElementById('remove').addEventListener('click', removeLastImage);
-document.getElementById('reverse').addEventListener('click', reverseGallery);
